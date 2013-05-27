65c34e1fcb0d964706fbc3f24d8010457669bfdb
Pseudo <arrayzelix+1@gmail.com>
2013-05-27 23:52:39 +0200

    Logic improvement.
    Fixed a would-be issue, demonstration of a dynamic sleep.

--- src/com/example/tutorial/ExampleBanking.java
+++ src/com/example/tutorial/ExampleBanking.java
@@ -1,25 +1,30 @@
-package com.example.tutorial;
- 
-import com.example.Node;
+package tutorialpackage;
+
 import org.hexbot.api.methods.Bank;
 import org.hexbot.api.methods.Inventory;
- 
+import org.hexbot.api.util.Time;
+import org.hexbot.api.util.Timer;
+
 public class ExampleBanking extends Node {
-    private final int JUNK = 666;
- 
-    @Override
-    public boolean activate() {
-        return Inventory.isFull(); //The node will only activate if the players inventory is full
-    }
-     
-    @Override
-    public void execute() {
-        if (Bank.isOpen()) {
-            if (Bank.deposit(JUNK, 0)) {
-                Bank.close();
-            } else {
-                Bank.open();
-            }
-        }
-    }
+
+ private final int JUNK = 666;
+
+	@Override
+	public boolean activate() {
+		return Inventory.isFull() && Bank.isOpen(); //The node will only activate the players inventory is full and the bank is open
+	}
+
+	@Override
+	public void execute() {
+		if (Inventory.contains(JUNK)) {
+			if (Bank.deposit(JUNK, 0)) {
+				Timer t = new Timer(3000); //Fail safe timer for dynamic sleep
+				while (t.isRunning() && Inventory.contains(JUNK)) { //While the timer is running, and the junk still exists inside the inventory, sleep. (Stops spamming of the 'junk).
+					Time.sleep(50, 150);
+				}
+				Bank.close(); //If the junk has deposited successfully (and the while loop has been exited) close the bank.
+			}
+		}
+	}
+
 }
